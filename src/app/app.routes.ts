a6073e2d63962e042bb9442908b4d0feb8114930
Denis Svetleishii <denissvetleishii@outlook.com>
2024-08-20 18:27:32 +0400

    feat: add routing to admin page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin',
+        loadComponent: () =>
+          import('./admin/admin.component').then(
+            (m) => m.AdminComponent,
+          ),
+      },
+];
