b7163fb43007e8c9518c2b60dad08675cafc9133
Denis Svetleishii <denissvetleishii@outlook.com>
2024-08-25 14:14:36 +0400

    refactor: add custom admin login into admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { tap } from 'rxjs';
 import { StationComponent } from './pages/stations/stations.component';
+import { ICreateAdmin } from './models/create-admin';
+import { AdminService } from './service/admin.service';
 
 @Component({
   selector: 'app-admin',
@@ -8,4 +11,22 @@ import { StationComponent } from './pages/stations/stations.component';
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
-export class AdminComponent {}
+export class AdminComponent {
+  private adminService = inject(AdminService);
+
+  readonly newAdmin: ICreateAdmin = {
+    email: 'admin@admin.com',
+    password: 'my-password',
+  };
+
+  constructor() {
+    this.adminService
+      .loginAdmin(this.newAdmin)
+      .pipe(
+        tap((response) => {
+          this.adminService.token$.next(response.token);
+        })
+      )
+      .subscribe();
+  }
+}
--- src/app/core/store/admin-store/effects/station-effect.service.ts
+++ src/app/core/store/admin-store/effects/station-effect.service.ts
@@ -1,9 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { AdminService } from '@app/admin/service/admin.service';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, concatMap, EMPTY, map, switchMap } from 'rxjs';
+import { catchError, EMPTY, map, switchMap } from 'rxjs';
 import { IStationList } from '@app/admin/models/station-list.model';
-import { ICreateAdmin } from '@app/admin/models/create-admin';
 import { StationsActions } from '../actions/stations.actions';
 
 @Injectable({
@@ -14,11 +13,6 @@ export class StationEffectService {
 
   private adminService = inject(AdminService);
 
-  readonly newAdmin: ICreateAdmin = {
-    email: 'admin@admin.com',
-    password: 'my-password',
-  };
-
   loadStations$ = createEffect(() =>
     this.actions$.pipe(
       ofType(StationsActions.loadStationList),
@@ -26,7 +20,7 @@ export class StationEffectService {
         this.adminService.getStationList().pipe(
           map((stations: IStationList[]) => StationsActions.loadStationsSuccess({ stations })),
           catchError((error) => {
-            console.error('Error loading videos:', error);
+            console.error('Error loading stations:', error);
             return EMPTY;
           })
         )
@@ -34,41 +28,29 @@ export class StationEffectService {
     )
   );
 
-  createStation$ = createEffect(() => {
-    return this.actions$.pipe(
+  createStation$ = createEffect(() =>
+    this.actions$.pipe(
       ofType(StationsActions.createNewStation),
       switchMap(({ station }) =>
-        this.adminService.loginAdmin(this.newAdmin).pipe(
-          concatMap((response) => {
-            this.adminService.token$.next(response.token);
-            return this.adminService.createNewStation(station);
-          }),
-          concatMap(() => {
-            return this.adminService.getStationList();
-          }),
-          map(() => StationsActions.loadStationList()),
+        this.adminService.createNewStation(station).pipe(
+          switchMap(() => this.adminService.getStationList()),
+          map((stations: IStationList[]) => StationsActions.loadStationsSuccess({ stations })),
           catchError((error) => {
-            console.error('Error loading videos:', error);
+            console.error('Error creating station:', error);
             return EMPTY;
           })
         )
       )
-    );
-  });
+    )
+  );
 
   deleteStation$ = createEffect(() =>
     this.actions$.pipe(
       ofType(StationsActions.deleteStation),
       switchMap(({ idStation }) =>
-        this.adminService.loginAdmin(this.newAdmin).pipe(
-          concatMap((response) => {
-            this.adminService.token$.next(response.token);
-            return this.adminService.deleteStation(idStation);
-          }),
-          concatMap(() => {
-            return this.adminService.getStationList();
-          }),
-          map(() => StationsActions.loadStationList()),
+        this.adminService.deleteStation(idStation).pipe(
+          switchMap(() => this.adminService.getStationList()),
+          map((stations: IStationList[]) => StationsActions.loadStationsSuccess({ stations })),
           catchError((error) => {
             console.error('Error deleting station:', error);
             return EMPTY;
