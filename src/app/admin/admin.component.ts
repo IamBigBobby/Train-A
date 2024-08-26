cf6250b626022c3249c6204a2d3ac56264ef6adc
Denis Svetleishii <denissvetleishii@outlook.com>
2024-08-26 11:51:42 +0400

    fix: fix auth token

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,9 +1,6 @@
-import { Component, inject, OnDestroy } from '@angular/core';
-import { Subscription, tap } from 'rxjs';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { StationComponent } from './pages/stations/stations.component';
-import { ICreateAdmin } from './models/create-admin';
-import { AdminService } from './service/admin.service';
 
 @Component({
   selector: 'app-admin',
@@ -12,30 +9,4 @@ import { AdminService } from './service/admin.service';
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
-export class AdminComponent implements OnDestroy {
-  private adminService = inject(AdminService);
-
-  private adminSub: Subscription | null = null;
-
-  readonly newAdmin: ICreateAdmin = {
-    email: 'admin@admin.com',
-    password: 'my-password',
-  };
-
-  constructor() {
-    this.adminSub = this.adminService
-      .loginAdmin(this.newAdmin)
-      .pipe(
-        tap((response) => {
-          this.adminService.token$.next(response.token);
-        })
-      )
-      .subscribe();
-  }
-
-  ngOnDestroy(): void {
-    if (this.adminSub) {
-      this.adminSub.unsubscribe();
-    }
-  }
-}
+export class AdminComponent {}
--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -4,7 +4,7 @@ export const AdminRoutes: Routes = [
   {
     path: '',
     redirectTo: 'station',
-    pathMatch: "full",
+    pathMatch: 'full',
   },
   {
     path: 'station',
