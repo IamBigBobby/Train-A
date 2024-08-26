63d75b5a62a0ec71a3e78f27a2148956780aa468
Magadanov <magadanov03@mail.ru>
2024-08-26 12:54:42 +0500

    feat: implement sign up page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './auth/pages/sign-up/sign-up.component';
+import { SignInComponent } from './auth/pages/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'signup', component: SignUpComponent },
+  { path: 'signin', component: SignInComponent },
+];
