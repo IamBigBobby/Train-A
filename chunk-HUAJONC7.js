60ab69ca491e098758a49473cec5c7b8e23f0978
Vitaly Fatkullin <vitalyasatka@gmail.com>
2024-09-04 17:39:19 +0500

    Auto-generated commit

--- chunk-HUAJONC7.js
+++
@@ -1,1 +0,0 @@
-import{c as e,d}from"./chunk-RR4QGK55.js";var r={loadRiderList:e("[Rider] Load Rider List",d()),loadRiderListSuccsess:e("[Rider] Load Rider List Succsess",d()),createRide:e("[Rider] Create Ride",d()),createRideSuccess:e("[Rider] Create Ride Success",d())};export{r as a};
